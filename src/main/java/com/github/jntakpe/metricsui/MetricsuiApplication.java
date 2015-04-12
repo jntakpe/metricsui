0f25286c59a7acffa9bfd7ecf868a1139bca9ca7
jntakpe <j.ntakpe@gmail.com>
2015-04-12 18:49:48 +0200

    Initial commit

---
+++ src/main/java/com/github/jntakpe/metricsui/MetricsuiApplication.java
@@ -0,0 +1,12 @@
+package com.github.jntakpe.metricsui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetricsuiApplication.class, args);
+    }
+}
